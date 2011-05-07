5474b55da07352d56a5b56c59f071886e0621caf
Chris Vaszauskas <chris.vaszauskas@gmail.com>
2011-05-06 22:33:22 -0500

    Improved structure and organization of main()
    ... and added boost::foreach! It's super neat.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <cstdio>
-#include <cassert>
-#include <map>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <set>
-#include <time.h>
+#include <string>
 #include <unistd.h>  // for sleep()
-#include <iomanip>
 
 #include <google/profiler.h>
+#include <boost/foreach.hpp>
 
 #include "array-hash.h"
 #include "hat-trie.h"
@@ -17,33 +13,38 @@
 using namespace std;
 using namespace stx;
 
+#define foreach BOOST_FOREACH
+
 template <class T>
 void print(const T &t) {
-    typename T::iterator it;
-    for (it = t.begin(); it != t.end(); ++it) {
-        cout << *it << endl;
+    // use boost's foreach function! woot!
+    foreach (const string &s, t) {
+        cout << s << endl;
     }
 }
 
-int main(int argc, char **args) {
-    std::ios_base::sync_with_stdio(false);  // speed up reading from stdin
+void stl() {
+    set<string> s;
+    string reader;
+    while (cin >> reader) {
+        s.insert(reader);
+    }
+}
 
-    ProfilerStart("profile/prof.prof");
+void mine_c() {
     hat_trie ht;
 
-//    string reader;
-//    while (cin >> reader) {
-//        ht.insert(reader);
-//    }
-
     // read entire file into main memory
-    FILE *f = fopen(args[1], "r");
+    FILE *f = stdin;
     fseek(f, 0, SEEK_END);
     int size = ftell(f);
     char *data = new char[size];
     fseek(f, 0, SEEK_SET);
     fread(data, 1, size, f);
     fclose(f);
+
+    // scan through the file and insert all the words into the
+    // trie
     char *start = data;
     for (int i = 0; i < size; ++i) {
         if (data[i] == '\n') {
@@ -52,11 +53,26 @@ int main(int argc, char **args) {
             start = data + i + 1;
         }
     }
-
-    //sleep(1000);
-
     //print(ht);
-    ProfilerStop();
+}
+
+void mine() {
+    hat_trie ht;
+    string reader;
+    while (cin >> reader) {
+        ht.insert(reader);
+    }
+}
+
+int main(int argc, char **args) {
+    std::ios_base::sync_with_stdio(false);  // speed up reading from stdin
+
+    //ProfilerStart("profile/prof.prof");
+    //stl();
+    mine();
+    //mine_c();
+    //ProfilerStop();
 
     return 0;
 }
+
