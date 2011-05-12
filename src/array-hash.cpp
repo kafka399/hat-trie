94be586e3483489a3876188f06ee42838cafbc97
Chris Vaszauskas <chris.vaszauskas@gmail.com>
2011-05-11 21:18:36 -0500

    Licensed *.cpp files under LGPL

--- src/array-hash.cpp
+++ src/array-hash.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2010-2011 Chris Vaszauskas and Tyler Richard
+ *
+ * This file is part of a HAT-trie implementation following the paper
+ * entitled "HAT-trie: A Cache-concious Trie-based Data Structure for
+ * Strings" by Nikolas Askitis and Ranjan Sinha.
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "array-hash.h"
 #include <cassert>
 #include <iostream>
